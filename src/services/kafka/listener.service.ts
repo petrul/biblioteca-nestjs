89c46c3fd09e599d3666bb93868797ade4ece8a2
Petru Dimulescu <petru.dimulescu@gmail.com>
2024-03-27 16:33:28 +0100

    ignore error

--- src/services/kafka/listener.service.ts
+++ src/services/kafka/listener.service.ts
@@ -27,13 +27,17 @@ export class ListenerService implements OnApplicationShutdown, OnModuleInit {
     });
     await this.consumer.run({
       eachMessage: (async ({ topic, partition, message }) => {
-        const asJson = message.value.toString();
-        const obj = JSON.parse(message.value.toString())
-        console.log(obj);
-        const opId = obj.id;
-        log(`starting vectorizing for ${obj.id}`, asJson);
-        await this.vectorizer.vectorize(opId);
-        log(`done vectorizing for ${obj.id}`, asJson);
+        try {
+          const asJson = message.value.toString();
+          const obj = JSON.parse(message.value.toString())
+          console.log(obj);
+          const opId = obj.id;
+          log(`starting vectorizing for ${obj.id}`, asJson);
+          await this.vectorizer.vectorize(opId);
+          log(`done vectorizing for ${obj.id}`, asJson);  
+        } catch(err: any) {
+          console.error('will ignore exception', err);
+        }
       }),
     });
   }
